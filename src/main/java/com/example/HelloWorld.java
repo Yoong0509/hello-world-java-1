1a4e69823a11e0d41885743caff0a1af9cd3985c
Lee <yoongyoong2016@1utar.my>
2026-04-06 15:50:13 +0800

    second commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,11 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, Student!"); //comment
     }
 
     public String getGreeting() {
         return "Hello, World!";
     }
 }
+    
